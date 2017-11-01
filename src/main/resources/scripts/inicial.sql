866bf4a09d21cd3ca56b9c2cd6ed7d3585757477
Saltoleto <fernandosaltoleto@hotmail.com>
2017-11-01 20:31:29 -0200

    Atualizações

---
+++ src/main/resources/scripts/inicial.sql
@@ -0,0 +1,6 @@
+
+
+insert into public.congregacao values (1,'Centro',000000-000,'Maringa','00.000.000/0000-00','Fernando Saltoleto','dsdsds@scdcx.com',
+'l kj 0000',15,true,'11','Assembleia Doutor Camargo','(44) 3447-3466')
+
+insert into public.usuario values(1,'ADMIN','admin@asdgdas.com','Saltoleto',false,false,'12345',1)
